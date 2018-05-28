6f5d9cc6c1a671f79f8c10e460dce0f7b74d0d6f
constantlyconfused <sam.hine27@gmail.com>
2018-05-29 00:39:17 +0100

    Add files via upload

---
+++ floaties/sketch.js
@@ -0,0 +1,103 @@
+var floatarray=[];
+var canvaswidth=800;
+var canvasheight=800;
+var numfloaties=15;
+function setup() {
+	createCanvas(canvaswidth,canvasheight);
+	
+	for(var i=0; i<numfloaties; i++){
+		var sfill=false;
+		var type;
+		
+		var randnum=random(0,1);
+		if (randnum<0.7){
+			type='circle';
+		}else if (randnum<0.8){
+			type='square';
+		}else if (randnum<0.9){
+			type='triangle';
+		}else{
+			type='cross';
+		}
+		
+		if (random(0,1)>0.5 && type=='circle'){
+			sfill=true;
+		}
+		
+		floatarray.push(new Floaty(type, sfill, random(15,30)));
+	}
+}
+
+function draw() {
+	background(unhex(['22','25','28']));
+	
+	for(var i=0; i<numfloaties; i++){
+		floatarray[i].update();
+		floatarray[i].render();
+		if (floatarray[i].offscreen()){
+			floatarray[i].vel.mult(-1);
+		}
+	}
+	
+	if (floatarray.size<numfloaties){
+		for(var i=0; i<numfloaties-floatarray.size; i++){
+		var sfill=true;
+		var type;
+		if (random(0,1)>0.5 && type=='circle'){
+			sfill=false;
+		}
+		var randnum=random(0,1);
+		if (randnum<0.4){
+			type='circle';
+		}else if (randnum<0.6){
+			type='square';
+		}else if (randnum<0.8){
+			type='triangle';
+		}else{
+			type='cross';
+		}
+		
+		floatarray.push(new Floaty(type, sfill, random(15,30)));
+		}
+	}
+}
+
+//circle, square, cross, triangle
+function Floaty(type, sfill, size){
+	this.pos = createVector(random(0,canvaswidth),random(0,canvasheight));
+	this.vel = createVector(random(-0.5,0.5),random(-.5,.5));
+	this.type = type;
+	this.fill = sfill;
+	this.size = size;
+	this.opacity = 1
+	
+	this.update = function(){
+		this.pos.add(this.vel);
+		
+	}
+	
+	this.render = function(){
+		if (this.fill){
+			fill('rgba(255,255,255,'+this.opacity+')');
+		}else{
+			noFill();
+		}
+		stroke('rgba(255,255,255,'+this.opacity+')');
+		strokeWeight(1);
+		if (this.type=="circle"){
+			ellipse(this.pos.x, this.pos.y, this.size, this.size);
+		}else if (this.type=="square"){
+			rect(this.pos.x, this.pos.y, this.size, this.size, 5);
+		}else if (this.type=="triangle"){
+			triangle(this.pos.x, this.pos.y, this.pos.x+this.size/2, this.pos.y-this.size, this.pos.x+this.size, this.pos.y);
+		}else if (this.type=="cross"){
+			strokeWeight(3);
+			line(this.pos.x-this.size/2, this.pos.y+this.size/2, this.pos.x+this.size/2, this.pos.y-this.size/2);
+			line(this.pos.x-this.size/2, this.pos.y-this.size/2, this.pos.x+this.size/2, this.pos.y+this.size/2);
+		}
+	}
+	
+	this.offscreen = function(){
+		return (this.pos.x < -this.size*1.5 || this.pos.x > canvaswidth+this.size*1.5 || this.pos.y < -this.size*1.5 || this.pos.y > canvasheight+this.size*1.5);
+	}
+}
